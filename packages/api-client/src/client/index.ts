2d252930af3938f203026f68f20df9190d103728
Fernando Cacenot <cacenot@gmail.com>
2026-02-02 18:34:22 -0300

    chore(api-client): release v0.3.3

--- packages/api-client/src/client/index.ts
+++ packages/api-client/src/client/index.ts
@@ -8,12 +8,17 @@
 export * from "./generated/auth/auth.js";
 // Skip enums - we have our own with labels in src/enums/
 // export * from "./generated/enums/enums.js";
+export * from "./generated/document-types/document-types.js";
 export * from "./generated/health/health.js";
 export * from "./generated/invitations/invitations.js";
 export * from "./generated/organization-users/organization-users.js";
 export * from "./generated/professionals/professionals.js";
 export * from "./generated/screening/screening.js";
+export * from "./generated/screening-alerts/screening-alerts.js";
+export * from "./generated/screening-documents/screening-documents.js";
+export * from "./generated/screening-process/screening-process.js";
 export * from "./generated/screening-public/screening-public.js";
+export * from "./generated/screening-steps/screening-steps.js";
 export * from "./generated/specialties/specialties.js";
 
 // Re-export all models (interfaces, types)
